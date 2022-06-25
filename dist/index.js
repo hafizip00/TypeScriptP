7f406967b7b72f34a7435862ec84119fe3d0e51e
hafizip00 <hafizip00@gmail.com>
2022-06-25 23:19:09 +0500

    BASICS OF TS

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let sales = 123456789;
+let course = "TypeScript";
+let is_published = true;
+let level;
+let numbers = [];
+let user = [1, "Mosh"];
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let sales = 123_456_789;
+let course = "TypeScript";
+let is_published = true;
+let level;
+
+let numbers:number[] = []
+let user : [number , string] = [1 , "Mosh"];
